0024ac533e61c16b2bc60fe9f541c38e3d0c242c
Bellamalwa <duchybella@gmail.com>
2024-02-18 07:46:34 -0800

    program that solves nqueens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+"""
+The N queens puzzle is the challenge of placing
+N non-attacking queens on an N × N chessboard.
+"""
+
+import sys
+
+
+def nqueens(n, y, board):
+    """
+    Method: nqueens - place n queens
+            on an n by n board so that
+            no queens are attacking any
+            others.
+    Parameters: n is an int that sets
+                board size and # of queens
+    Return: All possible solutions to
+            placement, in list of lists form.
+    """
+    for x in range(n):  # for each column in the chessboard
+        hold = 0  # holds the number of conflicts for the current column
+        for q in board:  # for each queen in the board
+            if x == q[1]:  # if the current col is the same as the queen's col
+                hold = 1  # there is a conflict
+                break  # break out of the loop
+            if y - x == q[0] - q[1]:
+                hold = 1
+                break
+            if x - q[1] == q[0] - y:
+                hold = 1
+                break
+        if hold == 0:
+            board.append([y, x])
+            if y != n - 1:
+                nqueens(n, y + 1, board)
+            else:
+                print(board)
+            del board[-1]
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except Exception:
+        print('N must be a number')
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    nqueens(n, 0, [])
+
+
+if __name__ == '__main__':
+    main()
